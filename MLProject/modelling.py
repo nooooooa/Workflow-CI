5303fd13ac2f325ee2ed506e32fb30e59f6f542f
Nicholas Ghonius <103248784+nooooooa@users.noreply.github.com>
2026-04-07 17:28:39 +0700

    Add files via upload

---
+++ MLProject/modelling.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import mlflow
+import mlflow.sklearn
+
+# autolog untuk basic requirement
+mlflow.autolog()
+
+# load dataset hasil preprocessing
+df = pd.read_csv("titanic_preprocessing.csv")
+
+# split fitur dan target
+X = df.drop("Survived", axis=1)
+y = df["Survived"]
+
+# split train test
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+with mlflow.start_run():
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    print("Accuracy:", acc)
